61000add60ce585c033a2fcee3458ba132cc7089
mr-dark-codex <darkcodm123@gmail.com>
2025-04-14 11:50:28 +0000

    update push

---
+++ 14-04-15/pivot_lead_val.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM 
+( SELECT -- T.TagName, 
+    F.VAL,
+    -- F.DateAndTime, 
+    DBO.getEquipmentName(T.TagName) AS [TAG],
+    SUBSTRING(T.TagName, 0, LEN(T.TagName) - CHARINDEX('\', REVERSE(T.TagName)) + 1) [Eqiupment]
+    FROM dbo.FloatTable_test F, DBO.TagTable T
+    WHERE T.TagIndex = F.TagIndex
+    AND dbo.getEquipmentName(T.TagName) IN ('Energy_App', 'PF', 'Energy')
+    AND MONTH(F.DateAndTime) = 12 
+    AND YEAR(F.DateAndTime) = 2020
+    AND F.Val > 0
+) AS S
+PIVOT 
+(
+    AVG(VAL)
+    FOR S.TAG IN ([PF], [ENERGY_APP], [ENERGY])
+) AS PIVOTTABLE
